05cb0f90931104fd3fd83a0ca3ddcb7f1d1e158b
Ahmad Damanhuri <daman@mhs.usk.ac.id>
2025-11-24 17:06:14 +0700

    add function io and header function io

---
+++ bigchallange/fileio/fileio.h
@@ -0,0 +1,16 @@
+#ifndef FILEIO_H
+#define FILEIO_H
+
+#include <stdio.h>
+#include <stdlib.h>
+
+// Kita include process.h agar fileio kenal struct 'AlphabetGroup'
+// Menggunakan "../" untuk mundur satu folder lalu masuk ke process
+#include "../process/process.h"
+
+//Prototype fungsi input/output
+int showMenu();
+void saveToBinary(AlphabetGroup data[], const char *filename);
+void readBinaryAndShow(const char *filename, int n);
+
+#endif
