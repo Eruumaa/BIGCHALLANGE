069db30dfca8dde5bd53e10bf0644eafd67c63cd
Ahmad Damanhuri <daman@mhs.usk.ac.id>
2025-11-25 11:06:42 +0700

    add save function

--- bigchallange/fileio/fileio.h
+++ bigchallange/fileio/fileio.h
@@ -8,9 +8,13 @@
 // Menggunakan "../" untuk mundur satu folder lalu masuk ke process
 #include "../process/process.h"
 
-//Prototype fungsi input/output
+// Prototype fungsi input/output
 int showMenu();
 void saveToBinary(AlphabetGroup data[], const char *filename);
 void readBinaryAndShow(const char *filename, int n);
+
+// Fungsi utilitas tampilan
+void clearScreen();
+void waitUser();
 
 #endif
